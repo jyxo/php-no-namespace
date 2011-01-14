62183b385dbbf0541cc96a424bc72b37258225a6
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-01-14 17:18:49 +0100

    Add Jyxo_String

---
+++ Jyxo/String.php
@@ -0,0 +1,325 @@
+<?php
+
+/**
+ * Jyxo PHP Library
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://github.com/jyxo/php/blob/master/license.txt
+ */
+
+/**
+ * Base class for common string operations.
+ *
+ * @category Jyxo
+ * @package Jyxo_String
+ * @copyright Copyright (c) 2005-2011 Jyxo, s.r.o.
+ * @license https://github.com/jyxo/php/blob/master/license.txt
+ * @author Jan Tichý
+ * @author Jakub Tománek
+ * @author Jaroslav Hanslík
+ */
+class Jyxo_String
+{
+	/**
+	 * Trims all words in a string longer than given length.
+	 * String is delimited by whitespaces.
+	 * If a word is trimmed, an "etc" is added at the end. Its length is also considered.
+	 *
+	 * @param string $string Processed string
+	 * @param integer $length Maximum word length
+	 * @param string $etc "etc" definition
+	 * @return string
+	 */
+	public static function cutWords($string, $length = 25, $etc = '...')
+	{
+		$length = (int) $length;
+
+		return preg_replace_callback('~[^\s]{' . $length . ',}~', function($matches) use ($length, $etc) {
+			return Jyxo_String::cut($matches[0], $length, $etc);
+		}, $string);
+	}
+
+	/**
+	 * Trims a string to given length.
+	 * Trims at word boundaries (all non-alphanumeric characters are considered delimiters).
+	 * If the given string is trimmed, an "etc" is added at the end. Its length is also considered.
+	 *
+	 * @param string $string Trimmed string
+	 * @param integer $max Maximum length
+	 * @param string $etc "etc" definition
+	 * @return string
+	 */
+	public static function cut($string, $max = 50, $etc = '...')
+	{
+		// Trim whitespace
+		$string = trim($string);
+
+		// No trimming is needed
+		if (mb_strlen($string) <= $max) {
+			return $string;
+		}
+
+		// Find out "etc" length
+		switch ($etc) {
+			case '&hellip;':
+				$etcLength = 1;
+				break;
+			default:
+				$etcLength = mb_strlen(html_entity_decode($etc));
+				break;
+		}
+
+		// Look for word boundaries
+		$search = mb_substr($string, 0, ($max - $etcLength) + 1);
+		if (preg_match('~[^\w\pL\pN]~u', $search)) {
+			// Boundary found
+			$string = preg_replace('~[^\w\pL\pN]*[\w\pL\pN]*$~uU', '', $search);
+		} else {
+			// No word boundary found, will trim in the middle of a word
+			$string = mb_substr($string, 0, $max - $etcLength);
+		}
+
+		// Add "etc" at the end
+		$string .= $etc;
+
+		return $string;
+	}
+
+	/**
+	 * Converts a string from UTF-8 to ISO-8859-2.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function utf2iso($string)
+	{
+		return iconv('UTF-8', 'ISO-8859-2//TRANSLIT', $string);
+	}
+
+	/**
+	 * Converts a string from UTF-8 to an identifier form.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function utf2ident($string)
+	{
+		// Convert to lowercase ASCII and than all non-alphanumeric characters to dashes
+		$ident = preg_replace('~[^a-z0-9]~', '-', strtolower(self::utf2ascii($string)));
+		// Remove multiple dashes and dashes on boundaries
+		return trim(preg_replace('~-+~', '-', $ident), '-');
+	}
+
+	/**
+	 * Converts a string from UTF-8 to ASCII.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function utf2ascii($string)
+	{
+		static $replace = array(
+			'á' => 'a', 'Á' => 'A', 'ä' => 'a', 'Ä' => 'A', 'â' => 'a', 'Â' => 'A', 'ă' => 'a', 'Ă' => 'A', 'ą' => 'a', 'Ą' => 'A',
+			'č' => 'c', 'Č' => 'C', 'ç' => 'c', 'Ç' => 'C', 'ć' => 'c', 'Ć' => 'C', 'ď' => 'd', 'Ď' => 'D', 'đ' => 'd', 'Đ' => 'D',
+			'é' => 'e', 'É' => 'E', 'ě' => 'e', 'Ě' => 'E', 'ë' => 'e', 'Ë' => 'E', 'ę' => 'e', 'Ę' => 'E', 'í' => 'i', 'Í' => 'I',
+			'î' => 'i', 'Î' => 'I', 'ł' => 'l', 'Ł' => 'L', 'ľ' => 'l', 'Ľ' => 'L', 'ĺ' => 'l', 'Ĺ' => 'L', 'ń' => 'n', 'Ń' => 'N',
+			'ň' => 'n', 'Ň' => 'N', 'ó' => 'o', 'Ó' => 'O', 'ô' => 'o', 'Ô' => 'O', 'ö' => 'o', 'Ö' => 'O', 'ő' => 'o', 'Ő' => 'O',
+			'o' => 'o', 'O' => 'O', 'ř' => 'r', 'Ř' => 'R', 'ŕ' => 'r', 'Ŕ' => 'R', 'š' => 's', 'Š' => 'S', 'ś' => 's', 'Ś' => 'S',
+			'ş' => 's', 'Ş' => 'S', 'ť' => 't', 'Ť' => 'T', 'ţ' => 't', 'Ţ' => 'T', 'ú' => 'u', 'Ú' => 'U', 'ů' => 'u', 'Ů' => 'U',
+			'ü' => 'u', 'Ü' => 'U', 'ű' => 'u', 'Ű' => 'U', 'ý' => 'y', 'Ý' => 'Y', 'ž' => 'z', 'Ž' => 'Z', 'ź' => 'z', 'Ź' => 'Z',
+			'ż' => 'z', 'Ż' => 'Z', 'ß' => 'ss', 'å' => 'a', 'Å' => 'A'
+		);
+		return strtr($string, $replace);
+	}
+
+	/**
+	 * Phonetical transcription of a Cyrillic string into ASCII.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function russian2ascii($string)
+	{
+		static $russian = array(
+			'КВ', 'кв', 'КС', 'кс', 'А', 'а', 'Б', 'б', 'Ц', 'ц', 'Д', 'д', 'Э', 'э', 'Е', 'е', 'Ф', 'ф', 'Г', 'г', 'Х', 'х',
+			'И', 'и', 'Й', 'й', 'К', 'к', 'Л', 'л', 'М', 'м', 'Н', 'н', 'О', 'о', 'П', 'п', 'Р', 'р', 'С', 'с', 'Т', 'т', 'У',
+			'у', 'В', 'в', 'В', 'в', 'Ы', 'ы', 'З', 'з', 'Ч', 'ч', 'Ш', 'ш', 'Щ', 'щ', 'Ж', 'ж', 'Я', 'я', 'Ю', 'ю', 'ъ', 'ь'
+		);
+		static $ascii = array(
+			'Q', 'q', 'X', 'x', 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I',
+			'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
+			'V', 'v', 'W', 'w', 'Y', 'y', 'Z', 'z', 'Ch', 'ch', 'Sh', 'sh', 'Sht', 'sht', 'Zh', 'zh', 'Ja', 'ja', 'Ju', 'ju'
+		);
+		return str_replace($russian, $ascii, $string);
+	}
+
+	/**
+	 * Converts a string from CP-1250 to ASCII.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function win2ascii($string)
+	{
+		return strtr($string,
+			"\xe1\xe4\xe8\xef\xe9\xec\xed\xbe\xe5\xf2\xf3\xf6\xf5\xf4\xf8\xe0\x9a\x9d\xfa\xf9\xfc\xfb\xfd\x9e"
+			. "\xc1\xc4\xc8\xcf\xc9\xcc\xcd\xbc\xc5\xd2\xd3\xd6\xd5\xd4\xd8\xc0\x8a\x8d\xda\xd9\xdc\xdb\xdd\x8e",
+			'aacdeeillnoooorrstuuuuyzAACDEEILLNOOOORRSTUUUUYZ'
+		);
+	}
+
+
+	/**
+	 * Converts a string from ISO-8859-2 to ASCII.
+	 *
+	 * @param string $string String to convert
+	 * @return string
+	 */
+	public static function iso2ascii($string)
+	{
+		return strtr($string,
+			"\xe1\xe4\xe8\xef\xe9\xec\xed\xb5\xe5\xf2\xf3\xf6\xf5\xf4\xf8\xe0\xb9\xbb\xfa\xf9\xfc\xfb\xfd\xbe"
+			. "\xc1\xc4\xc8\xcf\xc9\xcc\xcd\xa5\xc5\xd2\xd3\xd6\xd5\xd4\xd8\xc0\xa9\xab\xda\xd9\xdc\xdb\xdd\xae",
+			'aacdeeillnoooorrstuuuuyzAACDEEILLNOOOORRSTUUUUYZ'
+		);
+	}
+
+	/**
+	 * Generates a crc checksum same on 32 and 64-bit platforms.
+	 *
+	 * @param string $string Input string
+	 * @return integer
+	 */
+	public static function crc($string)
+	{
+		$crc = crc32($string);
+		if ($crc & 0x80000000) {
+			$crc ^= 0xffffffff;
+			$crc++;
+			$crc = -$crc;
+		}
+		return $crc;
+	}
+
+	/**
+	 * Generates a random string of given length.
+	 *
+	 * @param integer $length String length
+	 * @return string
+	 */
+	public static function random($length)
+	{
+		static $chars = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$random = '';
+		for ($i = 1; $i <= $length; $i++) {
+			$random .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+		}
+		return $random;
+	}
+
+	/**
+	 * Checks if the given string is valid UTF-8.
+	 *
+	 * @param string $string String to check
+	 * @return boolean
+	 */
+	public static function checkUtf($string)
+	{
+		return (bool) preg_match('~~u', $string);
+	}
+
+	/**
+	 * Transliterates or removes unknown UTF-8 characters from a string.
+	 *
+	 * @param string $string String to fix
+	 * @return string
+	 */
+	public static function fixUtf($string)
+	{
+		return @iconv('UTF-8', 'UTF-8//TRANSLIT//IGNORE', $string);
+	}
+
+	/**
+	 * Fixes and unifies line endings in a string.
+	 *
+	 * @param string $string String to fix
+	 * @param string $lineEnd Desired line ending
+	 * @return string
+	 */
+	public static function fixLineEnding($string, $lineEnd = "\n")
+	{
+		$string = str_replace("\r\n", "\n", $string);
+		$string = str_replace("\r", "\n", $string);
+		$string = str_replace("\n", $lineEnd, $string);
+
+		return $string;
+	}
+
+	/**
+	 * Obfuscates an email address.
+	 *
+	 * @param string $email Email address
+	 * @param string $comment Put a comment into the address
+	 * @return string
+	 */
+	public static function obfuscateEmail($email, $comment = false)
+	{
+		if ($comment) {
+			return str_replace('@', '&#64;<!---->', $email);
+		} else {
+			return str_replace('@', '&#64;', $email);
+		}
+	}
+
+	/**
+	 * Converts first character of a string to lowercase.
+	 * Works correctly with multibyte encodings.
+	 *
+	 * @param string $string Input string
+	 * @return string
+	 */
+	public static function lcfirst($string)
+	{
+		return mb_strtolower(mb_substr($string, 0, 1)) . mb_substr($string, 1);
+	}
+
+	/**
+	 * Htmlspecialchars function alias with some parameters automatically set.
+	 *
+	 * @param string $string Input string
+	 * @param integer $quoteStyle Quote style
+	 * @param boolean $doubleEncode Prevent from double encoding
+	 * @return string
+	 */
+	public static function escape($string, $quoteStyle = ENT_QUOTES, $doubleEncode = false)
+	{
+		return @htmlspecialchars($string, (int) $quoteStyle, 'utf-8', (bool) $doubleEncode);
+	}
+
+	/**
+	 * Converts given size in bytes to kB, MB, GB, TB or PB
+	 * and appends the appropriate unit.
+	 *
+	 * @param float $size Input size
+	 * @param string $decimalPoint Decimal point
+	 * @param string $thousandsSeparator Thousands separator
+	 * @return string
+	 */
+	public static function formatBytes($size, $decimalPoint = ',', $thousandsSeparator = ' ')
+	{
+		static $units = array('B', 'kB', 'MB', 'GB', 'TB', 'PB');
+		foreach ($units as $unit) {
+			if ($size < 1024) {
+				break;
+			}
+			$size = $size / 1024;
+		}
+
+		$decimals = ('B' === $unit) || ('kB' === $unit) ? 0 : 1;
+		return number_format($size, $decimals, $decimalPoint, $thousandsSeparator) . ' ' . $unit;
+	}
+}
---
+++ tests/Jyxo/StringTest.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ * Jyxo PHP Library
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file license.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://github.com/jyxo/php/blob/master/license.txt
+ */
+
+require_once __DIR__ . '/../bootstrap.php';
+
+/**
+ * String processing test.
+ *
+ * @copyright Copyright (c) 2005-2011 Jyxo, s.r.o.
+ * @license https://github.com/jyxo/php/blob/master/license.txt
+ * @author Jakub Tománek
+ * @author Jaroslav Hanslík
+ * @author Ondřej Nešpor
+ */
+class Jyxo_StringTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Tests string trimming.
+	 */
+	public function testCut()
+	{
+		// Trim on space
+		$this->assertEquals('žluťoučký kůň...', $this->checkStringCut('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+		// Trim on period
+		$this->assertEquals('žluťoučký kůň...', $this->checkStringCut('žluťoučký kůň.Příšerně úpěl ďábelské ódy'));
+		// Trim on period and space
+		$this->assertEquals('žluťoučký kůň...', $this->checkStringCut('žluťoučký kůň. Příšerně úpěl ďábelské ódy'));
+		// Trim on comma
+		$this->assertEquals('žluťoučký kůň...', $this->checkStringCut('žluťoučký kůň,příšerně úpěl ďábelské ódy'));
+		// Trim on semicolon
+		$this->assertEquals('žluťoučký kůň...', $this->checkStringCut('žluťoučký kůň;příšerně úpěl ďábelské ódy'));
+
+		// Word boundary just at the end
+		$this->assertEquals('abcdefghijklm...', $this->checkStringCut('abcdefghijklmno pqrst'));
+		$this->assertEquals('abcdefghijklm...', $this->checkStringCut('abcdefghijklmn opqrst'));
+		$this->assertEquals('abcdefghijklm...', $this->checkStringCut('abcdefghijklm nopqrst'));
+
+		// No word boundaries
+		$this->assertEquals('abcdefghijklm...', $this->checkStringCut('abcdefghijklmnopqrstuvwxyz'));
+
+		// Etc as HTML entity
+		$this->assertEquals('žluťoučký kůň&hellip;', $this->checkStringCut('žluťoučký kůň příšerně úpěl ďábelské ódy', 14, '&hellip;'));
+
+		// Short
+		$shorty = '1234567890';
+		$this->assertEquals($shorty, $this->checkStringCut($shorty));
+		$this->assertEquals('12...', $this->checkStringCut($shorty, 5));
+
+	}
+
+	/**
+	 * Tests word trimming.
+	 */
+	public function testCutWords()
+	{
+		$this->assertEquals('žluťoučký kůň příšerně úpěl ďábelské ódy', $this->checkStringWordCut('žluťoučký kůň příšerně úpěl ďábelské ódy', 10));
+		$this->assertEquals('žluťo... kůň příšerně úpěl ďábelské ódy', $this->checkStringWordCut('žluťoučký kůň příšerně úpěl ďábelské ódy', 8));
+		$this->assertEquals('žl... kůň př... úpěl ďá... ódy', $this->checkStringWordCut('žluťoučký kůň příšerně úpěl ďábelské ódy', 5));
+
+		// Word boundary just at the end
+		$this->assertEquals('abcdefghijk... pqrst', $this->checkStringWordCut('abcdefghijklmno pqrst', 14));
+		$this->assertEquals('abcdefghijklmn opqrst', $this->checkStringWordCut('abcdefghijklmn opqrst', 14));
+		$this->assertEquals('abcdefghijklm nopqrst', $this->checkStringWordCut('abcdefghijklm nopqrst', 14));
+
+		// Etc as HTML entity
+		$this->assertEquals('žluťouč&hellip; kůň příšerně úpěl ďábelské ódy', $this->checkStringWordCut('žluťoučký kůň příšerně úpěl ďábelské ódy', 8, '&hellip;'));
+
+		// Short
+		$shorty = '12345678';
+		$this->assertEquals($shorty, $this->checkStringWordCut($shorty));
+		$this->assertEquals('12...', $this->checkStringWordCut($shorty, 5));
+	}
+
+	/**
+	 * Checks one string.
+	 *
+	 * @param string $string Input string
+	 * @param integer $max Max length
+	 * @param string $etc "Etc" definition
+	 * @return string
+	 */
+	private function checkStringWordCut($string, $max = 8, $etc = '...')
+	{
+		$cut = Jyxo_String::cutWords($string, $max, $etc);
+
+		// &hellip; has length of 1
+		$cut2 = strtr(html_entity_decode($cut), array('&hellip;' => '.'));
+
+		$words = preg_split('~\s+~', $string);
+		$trimmedWords = preg_split('~\s+~', $cut2);
+
+		$this->assertEquals(count($trimmedWords), count($words));
+
+		foreach ($words as $i => $word) {
+			if (mb_strlen($word) <= $max) {
+				$this->assertEquals($word, $trimmedWords[$i], 'Word trimmed even though it was short enough');
+			} else {
+				$this->assertLessThanOrEqual($max, mb_strlen($trimmedWords[$i]));
+				$this->assertRegExp('~' . preg_quote($etc == '&hellip;' ? '.' : $etc) . '$~', $trimmedWords[$i], 'String does not end with ' . $etc);
+			}
+		}
+
+		return $cut;
+	}
+
+	/**
+	 * Checks one string.
+	 *
+	 * @param string $string Input string
+	 * @param integer $max Max length
+	 * @param string $etc "Etc" definition
+	 * @return string
+	 */
+	private function checkStringCut($string, $max = 16, $etc = '...')
+	{
+		$cut = Jyxo_String::cut($string, $max, $etc);
+		// &hellip; has length of 1
+		$cutLength = mb_strlen(strtr(html_entity_decode($cut), array('&hellip;' => '.')));
+		$this->assertLessThanOrEqual($max, $cutLength, 'String is longer');
+
+		if (mb_strlen($string) <= $max) {
+			$this->assertEquals($string, $cut, 'String trimmed even though it was short enough');
+		} else {
+			$this->assertRegExp('~' . preg_quote($etc) . '$~', $cut, 'String does not end with ' . $etc);
+		}
+		return $cut;
+	}
+
+	/**
+	 * Tests conversion functions.
+	 */
+	public function testConvert()
+	{
+		$this->assertEquals('abc', Jyxo_String::utf2iso('abc'));
+		$this->assertNotEquals('žluťoučký kůň příšerně úpěl ďábelské ódy', Jyxo_String::utf2iso('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+		$this->assertRegExp('~^[a-z0-9]([a-z0-9\-]*[a-z0-9])?$~', Jyxo_String::utf2ident('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+		$this->assertEquals('zlutoucky kun priserne upel dabelske ody', Jyxo_String::utf2ascii('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+		$this->assertEquals('zlutoucky kun priserne upel dabelske ody', Jyxo_String::win2ascii(file_get_contents(DIR_FILES . '/string/cp1250.txt')));
+		$this->assertEquals('zlutoucky kun priserne upel dabelske ody', Jyxo_String::iso2ascii(file_get_contents(DIR_FILES . '/string/iso-8859-2.txt')));
+		$this->assertEquals(file_get_contents(DIR_FILES . '/string/iso-8859-2.txt'), Jyxo_String::utf2iso('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+		$this->assertEquals('zlutoucky-kun-priserne-upel-dabelske-ody', Jyxo_String::utf2ident('?žluťoučký  +  kůň příšerně úpěl ďábelské ódy...'));
+		$this->assertEquals('Rossija', Jyxo_String::russian2ascii('Россия'));
+		$this->assertEquals('Gosudarstvennyj gimn Rossijskoj Federacii', Jyxo_String::russian2ascii('Государственный гимн Российской Федерации'));
+	}
+
+	/**
+	 * Test the crc generator.
+	 */
+	public function testCrc()
+	{
+		$this->assertSame(-662733300, Jyxo_String::crc('test'));
+		$this->assertSame(-33591962, Jyxo_String::crc('žluťoučký kůň příšerně úpěl ďábelské ódy'));
+	}
+
+	/**
+	 * Tests the random string generator.
+	 */
+	public function testRandom()
+	{
+		for ($i = 1; $i <= 32; $i++) {
+			$random = Jyxo_String::random($i);
+			$this->assertEquals($i, strlen($random));
+			$this->assertRegExp('~^[a-z0-9]+$~i', $random);
+		}
+	}
+
+	/**
+	 * Tests UTF-8 checking.
+	 */
+	public function testCheckUtf()
+	{
+		$this->assertTrue(Jyxo_String::checkUtf('žluťoučký kůň pěl ďábelské ódy'));
+		$this->assertTrue(Jyxo_String::checkUtf('Государственный гимн Российской Федерации'));
+		$this->assertFalse(Jyxo_String::checkUtf(file_get_contents(DIR_FILES . '/string/cp1250.txt')));
+		$this->assertFalse(Jyxo_String::checkUtf(file_get_contents(DIR_FILES . '/string/iso-8859-2.txt')));
+	}
+
+	/**
+	 * Tests UTF-8 fixing.
+	 */
+	public function testFixUtf()
+	{
+		$this->assertEquals('žluťoučký kůň pěl ďábelské ódy', Jyxo_String::fixUtf('žluťoučký kůň pěl ďábelské ódy'));
+		$this->assertEquals('Государственный гимн Российской Федерации', Jyxo_String::fixUtf('Государственный гимн Российской Федерации'));
+
+		$expected = 'glibc' === ICONV_IMPL ? '' : 'luouk k pern pl belsk ';
+		$this->assertEquals($expected, Jyxo_String::fixUtf(file_get_contents(DIR_FILES . '/string/cp1250.txt')));
+		$this->assertEquals($expected, Jyxo_String::fixUtf(file_get_contents(DIR_FILES . '/string/iso-8859-2.txt')));
+	}
+
+	/**
+	 * Tests line ending conversions.
+	 */
+	public function testFixLineEnding()
+	{
+		$tests = array(
+			"test\r\nžlutý\r\n",
+			"test\ržlutý\r",
+			"test\r\nžlutý\r",
+			"test\nžlutý\r",
+			"test\nžlutý\r\n"
+		);
+
+		// No line ending given
+		foreach ($tests as $test) {
+			$this->assertEquals("test\nžlutý\n", Jyxo_String::fixLineEnding($test));
+			$this->assertNotEquals($test, Jyxo_String::fixLineEnding($test));
+		}
+		$this->assertEquals("test\nžlutý\n", Jyxo_String::fixLineEnding("test\nžlutý\n"));
+
+		// Line ending given
+		foreach ($tests as $test) {
+			foreach (array("\n", "\r", "\r\n") as $ending) {
+				$this->assertEquals(sprintf('test%1$sžlutý%1$s', $ending), Jyxo_String::fixLineEnding($test, $ending));
+				$this->assertNotEquals("test\nžlutý\r\n", Jyxo_String::fixLineEnding($test, $ending));
+			}
+		}
+	}
+
+	/**
+	 * Tests email address obfuscation.
+	 */
+	public function testObfuscateEmail()
+	{
+		$email = 'example@example.com';
+
+		$this->assertEquals('example&#64;example.com', Jyxo_String::obfuscateEmail($email));
+		$this->assertEquals('example&#64;<!---->example.com', Jyxo_String::obfuscateEmail($email, true));
+	}
+
+	/**
+	 * Tests first letter lowercase.
+	 */
+	public function testLcfirst()
+	{
+		$this->assertEquals('žlutý kůň', Jyxo_String::lcfirst('Žlutý kůň'));
+		$this->assertEquals('žlutý kůň', Jyxo_String::lcfirst('žlutý kůň'));
+	}
+
+	/**
+	 * Tests special HTML characters escaping.
+	 */
+	public function testEscape()
+	{
+		$this->assertEquals('test &amp; test', Jyxo_String::escape('test & test'));
+		$this->assertEquals('&quot;test&quot; &amp; &#039;test&#039;', Jyxo_String::escape('"test" & \'test\''));
+		$this->assertEquals('&quot;test&quot; &amp; \'test\'', Jyxo_String::escape('"test" & \'test\'', ENT_COMPAT));
+		$this->assertEquals('"test" &amp; \'test\'', Jyxo_String::escape('"test" & \'test\'', ENT_NOQUOTES));
+		$this->assertEquals('test &amp; test', Jyxo_String::escape('test &amp; test'));
+		$this->assertEquals('test &amp;amp; test', Jyxo_String::escape('test &amp; test', ENT_QUOTES, true));
+	}
+
+	/**
+	 * Tests byte size conversion.
+	 */
+	public function testFormatBytes()
+	{
+		$this->assertEquals('11 B', Jyxo_String::formatBytes(10.5));
+		$this->assertEquals('11 B', Jyxo_String::formatBytes(10.5, '.'));
+		$this->assertEquals('11 B', Jyxo_String::formatBytes(10.5, '.', ','));
+
+		$this->assertEquals('1,0 GB', Jyxo_String::formatBytes(1073741824));
+		$this->assertEquals('1.0 GB', Jyxo_String::formatBytes(1073741824, '.'));
+		$this->assertEquals('10,240.0 PB', Jyxo_String::formatBytes(11805916207174113034240, '.', ','));
+	}
+}
